e91b8278d582c2fb896b2c03e7517edacff4078e
David Kuseh <davegru203@gmail.com>
2019-09-11 14:13:19 +0000

    set state and added effect hook useEffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [mediaUrl, setMediaUrl] = useState();
+  const [description, setDescription] = useState();
+  const [title, setTitle] = useState();
+  const [mediaType, setMediaType] = useState();
+  const [date, setDate] = useState("");
+
+  useEffect(()=> {
+   axios.get("https://api.nasa.gov/planetary/apod?api_key=h6gk8qlDpi9XRYbLiuRoagS4u6yAbIv0CxNm64Td")
+      .then(response => {
+        setMediaUrl(response.data.url);
+        setDescription(response.data.explanation);
+        setTitle(response.data.title);
+        setMediaType(response.data.media_type);
+        setDate(response.data.date);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun 🚀! 
       </p>
     </div>
   );
